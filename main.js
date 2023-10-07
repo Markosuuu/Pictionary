600a5a923c27c927c38dbad2fea86f3e22acc2b5
Marcos Diaz <101750266+Markosuuu@users.noreply.github.com>
2023-10-07 14:34:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,25 +2,47 @@ const cardCategories = document.querySelector(".card-categories");
 const card = document.querySelector(".card");
 
 categories = [
-    "Todo",
-    "Acciones",
-    "Objetos",
-    "Comidas",
-    "Peliculas",
-    "Series",
-    "Juegos",
-    "Animes",
-    "Libros"
-]
+  "Todo",
+  "Acciones",
+  "Objetos",
+  "Comidas",
+  "Peliculas",
+  "Series",
+  "Juegos",
+  "Animes",
+  "Libros"
+];
 
-for (let i = 0; i < categories.length; i++) {
+function createDisplayCards() {
+  for (let i = 0; i < categories.length; i++) {
     const cloneCard = card.cloneNode(true);
-    
-    cloneCard.querySelector('.card-title').textContent = categories[i];
-    cloneCard.querySelector('.card-img img').setAttribute('alt', categories[i]);
-    cloneCard.querySelector('.card-img img').setAttribute('src', "img/" + categories[i] + ".svg");
-
+  
+    cloneCard.querySelector(".card-title").textContent = categories[i];
+    cloneCard.querySelector(".card-img img").setAttribute("alt", categories[i]);
+    cloneCard.querySelector("a").setAttribute("data-category", categories[i]);
+    cloneCard
+      .querySelector(".card-img img")
+      .setAttribute("src", "img/" + categories[i] + ".svg");
+  
     cardCategories.appendChild(cloneCard);
+  }
+  
+  cardCategories.removeChild(card);
 }
 
-cardCategories.removeChild(card);
+function sendCategory() {
+  document.addEventListener('DOMContentLoaded', function() {
+    const anchorElements = document.querySelectorAll("a");
+    anchorElements.forEach(element => {
+        element.addEventListener("click", function(event) {
+          if (event.target.tagName === "DIV" || event.target.tagName === "IMG") {
+            const dato = element.getAttribute("data-category");
+            localStorage.setItem("dato", dato);
+          }
+        });
+    });
+  });
+}
+
+createDisplayCards()
+sendCategory()
